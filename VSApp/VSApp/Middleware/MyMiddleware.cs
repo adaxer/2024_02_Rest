2295d92c6586a22d7c7ba2fb924ec569746e8d13
Andreas Daxer <info@andreas-daxer.de>
2024-02-22 13:48:54 +0100

    Intro-Projekte

---
+++ VSApp/VSApp/Middleware/MyMiddleware.cs
@@ -0,0 +1,23 @@
+﻿
+namespace VSApp.Middleware;
+
+public class MyMiddleware
+{
+    private readonly RequestDelegate next;
+    private readonly ILogger<MyMiddleware> logger;
+
+    public MyMiddleware(RequestDelegate next, ILogger<MyMiddleware> logger)
+    {
+        this.next = next;
+        this.logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        logger.LogInformation("In MyMiddleware");
+        // Sorgfältig!
+
+        await next(context);
+        logger.LogInformation($"Response status: {context.Response.StatusCode}");
+    }
+}
---
+++ VSApp/VSApp/Middleware/MyMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace VSApp.Middleware;
+
+public static class MyMiddlewareExtensions
+{
+    public static IApplicationBuilder UseMyMiddleware(this IApplicationBuilder app)
+    {
+        // Initialisieren der eigenen Middleware
+        app.UseMiddleware<MyMiddleware>();
+        return app;
+    }
+}
